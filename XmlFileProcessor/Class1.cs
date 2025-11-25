34f368ac28aa1981507ab05a87a803e0cfb4945e
Greg <stepniewski.greg@gmail.com>
2025-11-26 00:18:09 +0100

    Moveing file-rated properties into Processor class.

--- XmlFileProcessor/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XmlFileProcessor
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ XmlFileProcessor/XmlFileProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace XmlFileProcessor
+{
+    /// <summary>
+    /// Main class for managing
+    /// </summary>
+    public class XmlFileProcessor
+    {
+        public string FileName { get; init; }
+        /// <summary>
+        /// File size in bytes.
+        /// </summary>
+        public long FileSize { get; init; }
+        /// <summary>
+        /// File size in megabytes.
+        /// </summary>
+        public long FileSizeMB => FileSize / 1024 / 1024;
+
+        public XmlFileProcessor(IBrowserFile file) {
+            if(file == null) throw new ArgumentNullException(nameof(file));
+
+            FileName = file.Name;
+            FileSize = file.Size;
+        }
+    }
+}
