2bdfdd899a7ef9889e56c18b768797f3bf437463
Greg <stepniewski.greg@gmail.com>
2025-11-26 00:29:40 +0100

    Specification of empty XmlFileProcessor.

--- XmlFileProcessor/XmlFileProcessor.cs
+++ XmlFileProcessor/XmlFileProcessor.cs
@@ -3,11 +3,11 @@
 namespace XmlFileProcessor
 {
     /// <summary>
-    /// Main class for managing
+    /// Main class for managing XML file processing.
     /// </summary>
     public class XmlFileProcessor
     {
-        public string FileName { get; init; }
+        public string FileName { get; init; } = string.Empty;
         /// <summary>
         /// File size in bytes.
         /// </summary>
@@ -17,11 +17,17 @@ namespace XmlFileProcessor
         /// </summary>
         public long FileSizeMB => FileSize / 1024 / 1024;
 
+
+        private XmlFileProcessor() { }
+
         public XmlFileProcessor(IBrowserFile file) {
-            if(file == null) throw new ArgumentNullException(nameof(file));
+            if (file == null) throw new ArgumentNullException(nameof(file));
 
             FileName = file.Name;
             FileSize = file.Size;
         }
+
+
+        public static readonly XmlFileProcessor Empty = new() { FileName = "<None>", FileSize = 0 };
     }
 }
